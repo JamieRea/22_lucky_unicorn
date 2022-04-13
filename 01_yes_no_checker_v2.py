d7b0ae4cbe6372ab2e6b7e96930ab2ff510a886a
JamieRea <rea.jamie@awatapu.school.nz>
2022-04-14 10:54:54 +1200

    Created a new version of the code that uses language from the tutorial.

---
+++ 01_yes_no_checker_v2.py
@@ -0,0 +1,22 @@
+#Function goes here.
+def yes_no(question):
+    valid = False
+    while not valid:
+        # Ask user if they have played before.
+        response = input(question).lower().strip()
+        # If yes, skip the instructions.
+        if response == 'yes' or response == 'y':
+            response = "yes"
+            return response
+        # If no, display the instructions.
+        elif response == "no" or response == "n":
+            response == "no"
+            return response
+        #If other, rerun the code.
+        else:
+            print("You need to enter yes or no")
+# Main routine goes here...
+show_instructions = yes_no("Have you played this game before? ")
+print("You chose {}".format(show_instructions))
+
+
